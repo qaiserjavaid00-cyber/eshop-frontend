b167c8a03ebd4420c1aa291a4e882e11e629d82f
qauser.javaid00 <qaiser.javaid00@gmail.com>
2026-03-13 14:47:35 +0500

    Updated UI

--- src/hooks/useHomeRatings.js
+++ src/hooks/useHomeRatings.js
@@ -2,8 +2,8 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000";
-// const BASE_URL = "https://eshop-backend-production-edc1.up.railway.app";
+// const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://eshop-backend-production-edc1.up.railway.app";
 
 const getHomeRatingsAPI = async () => {
     const { data } = await axios.get(`${BASE_URL}/product/products/ratings`);
